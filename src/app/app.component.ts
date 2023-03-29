5c9150fce50bd274881487144459da81d2313da4
emraoruc <emraoruc@gmail.com>
2023-03-29 22:56:35 +0300

    to do list done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  title = 'To-Do-List';
+  nameModel:string = "";
+  selected: any= "";
+
+  listItem = [
+    {id:1,name:'List Item 1'},
+  ]
+
+  //listItem: string[] = ['List Item 1'];
+$event: any;
+item: any;
+
+
+  deleteItem(ev: Event, item: any) {
+    ev.stopPropagation();
+    ev.preventDefault();
+    this.listItem = this.listItem.filter(o => o !== item);
+    console.log(this.listItem)
+  }
+  addItem(){
+    if(this.nameModel != '' && this.nameModel != null){
+      let nextItem = this.listItem.length;
+      this.listItem.push({
+        id: nextItem,
+        name:this.nameModel
+      });
+      this.nameModel = '';
+    }
+  }
+
+  onNgModelChange($event: any,item: any){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule,     
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatDialogModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
